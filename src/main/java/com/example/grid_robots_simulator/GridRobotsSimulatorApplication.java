c6c0f75a1fb49ea091a6b6208c96184ffef29a0c
Álvaro Pérez <alvaropdp@APDP701929.local>
2026-01-26 16:05:12 +0100

    chore: initial commit

---
+++ src/main/java/com/example/grid_robots_simulator/GridRobotsSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.grid_robots_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridRobotsSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridRobotsSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grid_robots_simulator/GridRobotsSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grid_robots_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridRobotsSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
